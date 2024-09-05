8e18b799a897b33d5bd7716500b7f026eb5272e9
Andrei.Makash <andrei.makash@yandex.ru>
2024-09-05 20:22:23 +0300

    все права защищены Footer.scc

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import './Footer.css';
+
+function Footer() {
+  return (
+    <div className="Footer">
+    <p> &copy;2024.Все права защищены. </p>
+    
+    </div>
+  );
+}
+
+export default Footer;

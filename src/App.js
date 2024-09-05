22902af737ae52991cac18397e1aaab6fb388271
Andrei.Makash <andrei.makash@yandex.ru>
2024-09-05 22:02:43 +0300

    создаем Name.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
 import Footer from './components/Footer'
+import Name from './components/Name'
 function App() {
   return (
     <div className="App">
+     <Name /> 
     <Footer />
     </div>
   );
